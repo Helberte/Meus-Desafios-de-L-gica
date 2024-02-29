ac9016f2647ab37621f5cf6f7284419f2542b772
Helberte Costa Arruda <helberteads@gmail.com>
2024-02-29 09:01:52 -0400

    Resolvido desafio do canguru

---
+++ Canguru/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Desafio
+https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true 
+*/
+
+Console.WriteLine(Canguru.Kangaroo(2, 1, 1, 2));
+
+
+public class Canguru
+{
+    public static string Kangaroo(int x1, int v1, int x2, int v2)
+    {
+        int posicaoAtual01 = x1;
+        int posicaoAtual02 = x2;
+
+        // cada iteração é um salto
+        // ao final de cada salto é preciso verificar se eles se encontraram
+        for (int i = 0; i < 10000; i++)
+        {
+            posicaoAtual01 += v1;
+            posicaoAtual02 += v2;
+
+            if (posicaoAtual01.Equals(posicaoAtual02)) return "YES";
+        }
+
+        return "NO";
+    }
+}
